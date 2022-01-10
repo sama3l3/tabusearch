4218d9176203d249a229955c962e7e6d1bfd8a82
Picca93 <fr.nicastro@austech-italy.com>
2022-01-10 17:55:33 +0100

    added gui

--- tabu/src/Main.java
+++ tabu/src/Main.java
@@ -1,8 +1,71 @@
-public class Main {
-    public static void main(String[] args) {
-        Read r= new Read();
-        r.read("prova.txt");
-        TabuSearch t= new TabuSearch(r.getJobs());
-        t.localopt();
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Main extends JFrame {
+    private static File file;
+    private static final Read r= new Read();
+    private static final Path path = Path.of(System.getProperty("user.home"));
+
+    /*
+        Componenti swing per grafica ( JFrame in stack senza griglia, per modificare
+                                        usare x e y)
+     */
+
+    private static final JFrame f=new JFrame("Tabu Search Algorithm");
+    private static final JButton b = new JButton("Calcola Ottimo");
+    private static final JButton filechooser = new JButton("Scegli file ...");
+    private static final JCheckBox max = new JCheckBox("Max");
+    private static final JCheckBox min = new JCheckBox("Min");
+    private static final JFileChooser chooser = new JFileChooser(String.valueOf(path));
+
+
+    public static void main(String[] args)  {
+
+        /*
+        Bounds componenti
+         */
+        b.setBounds(100, 200, 150, 40);
+        filechooser.setBounds(100,50,150, 40);
+        min.setBounds(80, 250 , 50, 50);
+        max.setBounds(220, 250 , 50, 50);
+
+        /*
+        aggiunti componenti al JFrame
+         */
+        f.add(filechooser);
+        f.add(b);
+        f.setSize(350,350);
+        f.add(min);
+        f.add(max);
+
+        /*
+        layout settato a null e reso visibile il frame
+         */
+        f.setLayout(null);
+        f.setVisible(true);
+
+        /*
+        link JFileChooser con bottone
+         */
+        filechooser.addActionListener(e -> {
+            chooser.showOpenDialog(null);
+            file = chooser.getSelectedFile();
+        });
+
+        /*
+        attivazione localopt attraverso bottone "calcola ottimo"
+         */
+        b.addActionListener(e -> {
+            r.read(file.getPath());
+            TabuSearch t= new TabuSearch(r.getJobs());
+            t.localopt();
+            try {
+                Runtime.getRuntime().exec("explorer.exe /select," + file.getPath()); //apre esplora risorse dopo aver elaborato
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
